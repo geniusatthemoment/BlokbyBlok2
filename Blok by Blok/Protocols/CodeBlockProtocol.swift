cfd642b3c9b0a48eff1409edb0c71658cddec79e
Sergey Romaov <114457112+geniusatthemoment@users.noreply.github.com>
2025-05-31 10:23:07 +0700

    Update CodeBlockProtocol.swift

--- Blok by Blok/Protocols/CodeBlockProtocol.swift
+++ Blok by Blok/Protocols/CodeBlockProtocol.swift
@@ -3,5 +3,6 @@ import CoreTransferable
 
 protocol CodeBlockProtocol: Identifiable, Codable, Transferable, Equatable {
     var id: UUID { get }
-    var codeStr: String { get set}
+    var codeStr: String { get }
 }
+
